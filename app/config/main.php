f860e14adf640aedf6e05cb8af9ed2b2caada564
Khalil Majdalawi <khalil.majdalawi@gmail.com>
2023-04-08 20:57:08 +0300

    removing secret

--- app/config/main.php
+++ app/config/main.php
@@ -5,7 +5,7 @@ return [
         'port' => 3306,
         'dbname' => 'purephp',
         'username' => 'root',
-        'password' => 'letmein123',
+        'password' => '',
         'charset' => 'utf8mb4'
     ],
 ];
